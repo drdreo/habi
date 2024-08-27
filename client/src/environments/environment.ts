363f99d7c9e795373b1141f5aa33584ac0848577
DrDreo <code@anderas-hahn.at>
2024-08-27 22:23:16 +0200

    update prod auth

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,9 +3,9 @@ import { AppEnvironment } from "./env.type";
 export const environment: AppEnvironment = {
     production: true,
     auth0: {
-        domain: "",
-        clientId: "",
-        audience: ""
+        domain: "drdreo.eu.auth0.com",
+        clientId: "sBu4MMLZKo2G4XbJV3XHnLvkMAyZyGF8",
+        audience: "habit-api-dev.drdreo.com"
     },
     origins: {
         api: ""
