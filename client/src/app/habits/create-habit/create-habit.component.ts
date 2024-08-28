4066a16868d970bee64ea1932ab5228a6d90938b
DrDreo <code@anderas-hahn.at>
2024-08-28 22:25:46 +0200

    add create habit stuff

--- client/src/app/habits/create-habit/create-habit.component.ts
+++ client/src/app/habits/create-habit/create-habit.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
 import { MatButton } from "@angular/material/button";
 import { MatButtonToggleModule } from "@angular/material/button-toggle";
 import { provideNativeDateAdapter } from "@angular/material/core";
@@ -20,7 +20,8 @@ import { MatInput } from "@angular/material/input";
         MatIcon,
         MatInput,
         MatButton,
-        MatDatepickerModule
+        MatDatepickerModule,
+        FormsModule
     ],
     providers: [provideNativeDateAdapter()],
     templateUrl: "./create-habit.component.html",
@@ -28,13 +29,21 @@ import { MatInput } from "@angular/material/input";
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CreateHabitComponent {
-    nameControl = new FormControl("");
-    descriptionControl = new FormControl("");
-    unitControl = new FormControl("");
-    frequencyControl = new FormControl("daily");
-    typeControl = new FormControl("good");
-    targetMetricTypeControl = new FormControl<TargetMetricType>("quantity");
-    targetMetricValueControl = new FormControl();
+    habitForm = new FormGroup({
+        name: new FormControl("", Validators.required),
+        description: new FormControl(""),
+        frequency: new FormControl("daily", Validators.required),
+        targetMetric: new FormGroup({
+            type: new FormControl<TargetMetricType>("quantity"),
+            value: new FormControl(null, Validators.required),
+            unit: new FormControl("")
+        }),
+        type: new FormControl("good", Validators.required)
+    });
+
+    createHabit() {
+        console.log(this.habitForm);
+    }
 }
 
 type TargetMetricType = "quantity" | "duration" | "date" | null;
