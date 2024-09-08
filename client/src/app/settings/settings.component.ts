43fefc4979c64f966bcc7d17e35a3ea65e84a590
DrDreo <code@drdreo.com>
2024-09-08 12:55:26 +0200

    dark theme side track

--- client/src/app/settings/settings.component.ts
+++ client/src/app/settings/settings.component.ts
@@ -1,12 +1,26 @@
 import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
+import { MatSlideToggleChange, MatSlideToggleModule } from "@angular/material/slide-toggle";
 
 @Component({
     selector: "app-settings",
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, MatSlideToggleModule],
     templateUrl: "./settings.component.html",
     styleUrl: "./settings.component.scss",
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SettingsComponent {}
+export class SettingsComponent {
+    darkModeEnabled = signal(localStorage.getItem("darkMode") === "true");
+
+    toggleTheme($event: MatSlideToggleChange) {
+        const darkMode = $event.checked;
+        if (darkMode) {
+            document.body.classList.add("dark-mode");
+            localStorage.setItem("darkMode", "true");
+        } else {
+            document.body.classList.remove("dark-mode");
+            localStorage.setItem("darkMode", "false");
+        }
+    }
+}
