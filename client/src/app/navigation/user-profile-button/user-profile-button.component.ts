3fb663235d42e4cfa95b938a28e60308fa1c8c88
DrDreo <code@drdreo.com>
2024-09-17 23:09:36 +0200

    minor UI update

--- client/src/app/navigation/user-profile-button/user-profile-button.component.ts
+++ client/src/app/navigation/user-profile-button/user-profile-button.component.ts
@@ -1,15 +1,23 @@
-import { CommonModule } from "@angular/common";
+import { CommonModule, DOCUMENT } from "@angular/common";
 import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import { MatFabButton, MatIconButton } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
+import { MatMenu, MatMenuItem, MatMenuModule } from "@angular/material/menu";
 import { AuthService } from "@auth0/auth0-angular";
 
 @Component({
     selector: "user-profile-button",
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, MatIcon, MatMenu, MatMenuItem, MatIconButton, MatMenuModule, MatFabButton],
     templateUrl: "./user-profile-button.component.html",
     styleUrl: "./user-profile-button.component.scss",
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserProfileButtonComponent {
     auth = inject(AuthService);
+    private readonly document = inject(DOCUMENT);
+
+    logout() {
+        this.auth.logout({ logoutParams: { returnTo: this.document.location.origin } });
+    }
 }
